d703f8abec035900e12bdde8a0ef29f1697a4d91
BenserSPH <github.bensersph@gmail.com>
2026-09-04 21:07:19 +0200

    Math file method added "add", "sub", "div", "mul"

--- taschenrechner/src/taschenrechner/Math.java
+++ taschenrechner/src/taschenrechner/Math.java
@@ -2,8 +2,23 @@ package taschenrechner;
 
 public class Math {
 
+	public Math() {
+		
+	}
 	
 	public double add(double zahl1, double zahl2) {
 		return zahl1 + zahl2;
+	}
+	
+	public double sub(double zahl1, double zahl2) {
+		return zahl1 - zahl2;
+	}
+	
+	public double div(double zahl1, double zahl2) {
+		return zahl1 / zahl2;
+	}
+	
+	public double mul(double zahl1, double zahl2) {
+		return zahl1 * zahl2;
 	}
 }
